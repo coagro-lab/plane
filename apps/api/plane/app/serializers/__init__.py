a6da3f245e61968af69062fe3d307185573c852e
Leandro Ademar <leandro.ademars@gmail.com>
2025-08-20 12:14:31 -0300

    feat: remover importações não utilizadas e reorganizar o módulo de serializers

--- apps/api/plane/app/serializers/__init__.py
+++ apps/api/plane/app/serializers/__init__.py
@@ -1,4 +1,4 @@
-from .base import BaseSerializer, DynamicBaseSerializer
+from .base import BaseSerializer
 from .user import (
     UserSerializer,
     UserLiteSerializer,
@@ -23,7 +23,6 @@ from .workspace import (
     WorkspaceRecentVisitSerializer,
     WorkspaceHomePreferenceSerializer,
     StickySerializer,
-    WorkspaceUserMeSerializer,
 )
 from .project import (
     ProjectSerializer,
@@ -34,6 +33,7 @@ from .project import (
     ProjectIdentifierSerializer,
     ProjectLiteSerializer,
     ProjectMemberLiteSerializer,
+    DeployBoardSerializer,
     ProjectMemberAdminSerializer,
     ProjectPublicMemberSerializer,
     ProjectMemberRoleSerializer,
@@ -45,7 +45,6 @@ from .cycle import (
     CycleIssueSerializer,
     CycleWriteSerializer,
     CycleUserPropertiesSerializer,
-    EntityProgressSerializer,
 )
 from .asset import FileAssetSerializer
 from .issue import (
@@ -76,7 +75,6 @@ from .issue import (
     IssueVersionDetailSerializer,
     IssueDescriptionVersionDetailSerializer,
     IssueListDetailSerializer,
-    IssueDuplicateSerializer,
 )
 
 from .module import (
@@ -95,11 +93,11 @@ from .importer import ImporterSerializer
 from .page import (
     PageSerializer,
     PageLogSerializer,
-    PageLiteSerializer,
+    SubPageSerializer,
     PageDetailSerializer,
     PageVersionSerializer,
+    PageBinaryUpdateSerializer,
     PageVersionDetailSerializer,
-    PageUserSerializer,
 )
 
 from .estimate import (
@@ -132,14 +130,3 @@ from .draft import (
     DraftIssueSerializer,
     DraftIssueDetailSerializer,
 )
-from .integration import (
-    IntegrationSerializer,
-    WorkspaceIntegrationSerializer,
-    GithubIssueSyncSerializer,
-    GithubRepositorySerializer,
-    GithubRepositorySyncSerializer,
-    GithubCommentSyncSerializer,
-    SlackProjectSyncSerializer,
-)
-
-from .deploy_board import DeployBoardSerializer
